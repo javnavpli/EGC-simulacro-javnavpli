633286980551ee861835eb63b13b72c9a53afa3f
javsolgar <javsolgar@alum.us.es>
2020-12-08 19:25:24 +0100

    Añadido algoritmo de D'hont

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,6 +16,28 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def dhont(self, options, seats):
+        out = []
+
+        for opt in options:
+            out.append({
+                **opt,
+                'postproc': opt['votes'],
+            });
+
+        asientos = 0
+        while asientos <= seats:
+            cocientes = []
+            for i in range(len(out)):
+                cocientes.append(out[i]['votes'] / out[i]['postproc'] + 1)
+
+                ganador = cocientes.index(max(cocientes))
+                out[ganador]['postproc'] = out[ganador]['postproc'] + 1
+                asientos += 1
+
+        out.sort(key=lambda x: -x['postproc'])
+        return Response(out)
+
     def post(self, request):
         """
          * type: IDENTITY | DHONT
@@ -27,17 +49,17 @@ class PostProcView(APIView):
              ...extraparams
             }
            ]
-	 * seats: int
+	    * seats: int
         """
 
         t = request.data.get('type')
         opts = request.data.get('options', [])
-	s = request.data.get('seats')
+        s = request.data.get('seats')
 
         if t == 'IDENTITY':
             return self.identity(opts)
 
-	elif t == 'DHONT':
-	    return self.dhont(opts,s)
+        elif t == 'DHONT':
+            return self.dhont(opts, s)
 
         return Response({})
