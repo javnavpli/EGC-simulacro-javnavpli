872eb0b2f5714a71ea3e54e1a8d50ed2e3026a68
javsolgar <javsolgar@alum.us.es>
2020-12-12 12:48:10 +0100

    Añadida prueba de d'hont 2

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -19,22 +19,22 @@ class PostProcTestCase(APITestCase):
         data = {
             'type': 'IDENTITY',
             'options': [
-                { 'option': 'Option 1', 'number': 1, 'votes': 5 },
-                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
-                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
-                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
-                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
-                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+                {'option': 'Option 1', 'number': 1, 'votes': 5},
+                {'option': 'Option 2', 'number': 2, 'votes': 0},
+                {'option': 'Option 3', 'number': 3, 'votes': 3},
+                {'option': 'Option 4', 'number': 4, 'votes': 2},
+                {'option': 'Option 5', 'number': 5, 'votes': 5},
+                {'option': 'Option 6', 'number': 6, 'votes': 1},
             ]
         }
 
         expected_result = [
-            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 5 },
-            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 5 },
-            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 3 },
-            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 2 },
-            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1 },
-            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+            {'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 5},
+            {'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 5},
+            {'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 3},
+            {'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 2},
+            {'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1},
+            {'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0},
         ]
 
         response = self.client.post('/postproc/', data, format='json')
@@ -42,6 +42,8 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+
         
     def test_dhont1(self):
         data = {
@@ -63,6 +65,39 @@ class PostProcTestCase(APITestCase):
             { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 0 },
             { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 0 },
             { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_dhont_2(self):
+        seats = 5
+        data = {
+            'type': 'DHONT',
+            'seats': seats,
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'votes': 10},
+                {'option': 'Option 2', 'number': 2, 'votes': 0},
+                {'option': 'Option 3', 'number': 3, 'votes': 6},
+                {'option': 'Option 4', 'number': 4, 'votes': 4},
+                {'option': 'Option 5', 'number': 5, 'votes': 10},
+                {'option': 'Option 6', 'number': 6, 'votes': 2},
+                {'option': 'Option 7', 'number': 7, 'votes': 6},
+
+            ]
+        }
+
+        expected_result = [
+            {'option': 'Option 1', 'number': 1, 'votes': 10, 'postproc': 2},
+            {'option': 'Option 5', 'number': 5, 'votes': 10, 'postproc': 1},
+            {'option': 'Option 3', 'number': 3, 'votes': 6, 'postproc': 1},
+            {'option': 'Option 7', 'number': 7, 'votes': 6, 'postproc': 1},
+            {'option': 'Option 4', 'number': 4, 'votes': 4, 'postproc': 0},
+            {'option': 'Option 6', 'number': 6, 'votes': 2, 'postproc': 0},
+            {'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0},
         ]
 
         response = self.client.post('/postproc/', data, format='json')
