0db48eabe84fd987eb5454a245b0045b8bd691a4
marlasesc <marlasesc@alum.us.es>
2020-12-13 12:50:21 +0100

    Añadido algoritmo mayoria relativa

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,6 +16,27 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def relativa(self, opts):
+        out= []
+        numvotos=0
+        for opt in options:
+            numvotos=opt['votes']+numvotos
+            out.append({
+                **opt,
+                'postproc':opt['votes']
+            })
+
+        while len(out)>1:
+            if(max(cocientes)>0.5):
+                cocientes = []
+                for i in range(len(out)):
+                    cocientes.append(out[i]['votes']/numvotos)
+                    perdedor=cocientes.index(min(cocientes))
+                    del cocientes[perdedor]
+            else:
+                out.sort(key=lambda x:-x['votes'])
+                return Response(out)
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -29,7 +50,7 @@ class PostProcView(APIView):
            ]
         """
 
-        t = request.data.get('type', 'IDENTITY')
+        t = request.data.get('type')
         opts = request.data.get('options', [])
 
         if t == 'IDENTITY':
