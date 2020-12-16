062a94f87a5c1cecb3cabff8bba7083c99ef1435
julsoutos <julsoutos@alum.us.es>
2020-12-16 17:42:30 +0100

    Editado settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
         'USER': 'decide',
-        'PASSWORD': 'decide',
+        'PASSWORD': 'decide2020',
         'HOST': 'localhost',
         'PORT': '5432',
     }
