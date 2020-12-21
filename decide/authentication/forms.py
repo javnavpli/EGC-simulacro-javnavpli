5682bd64c7e7845f4ab5a7b7f7ff681978b4163f
julsoutos <julsoutos@alum.us.es>
2020-12-21 10:18:06 +0100

    Creación modelo de extensión de user

--- decide/authentication/forms.py
+++ decide/authentication/forms.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
 
 
--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+#Extra: Atributos extras para el modelo User
+class Extra(models.Model)
+    id = models.AutoField(primary_key=True)
+    phone = models.CharField(required = True, max_length = 100, label='Telefono')
+    double_authentication = models.BooleanField(required=True, label = 'Doble autenticación')
+    
