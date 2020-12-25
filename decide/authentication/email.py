3401f6ec2f86941129abd8bdd2c8ae0ba54f5196
Antonio Suárez Bono <antsuabon@alum.us.es>
2020-12-25 23:48:34 +0100

    Función para enviar emails
    Versión inicial de la función utilizada para enviar correos
    electrónicos. Se utiliza una configuración de prueba sin conectarse con
    el servidor de correo.

---
+++ decide/authentication/email.py
@@ -0,0 +1,23 @@
+from django.core.mail import EmailMultiAlternatives, BadHeaderError
+from django.template.loader import get_template
+from django.template import Context
+
+def send_mail_with_token(to_email, token):
+    subject = 'Decide - Correo de prueba'
+    from_email = 'Decide <do_not_reply@example.com>'
+
+    cont = { 
+            'usuario': 'username de prueba',
+            'token': token
+        }
+
+    msg_plain = get_template('email.txt').render(cont)
+    msg_html = get_template('email.html').render(cont)
+
+    try:
+        msg = EmailMultiAlternatives(subject, msg_plain, from_email, [to_email])
+        msg.attach_alternative(msg_html, "text/html")
+        msg.send()
+    except BadHeaderError:
+        pass
+    
