713f3f94c7665d5c9be2e5f8edea344d0f3dce1d
julsoutos <julsoutos@alum.us.es>
2020-12-26 10:36:16 +0100

    Editado archivo travis para selenium

--- decide/authentication/test_formularioRegistro.py
+++ decide/authentication/test_formularioRegistro.py
@@ -6,6 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 from base.tests import BaseTestCase
 
