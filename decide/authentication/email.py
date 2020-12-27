7925910bb5ceb07145e2232b8e36a6cfd593db4a
Antonio Suárez Bono <antsuabon@alum.us.es>
2020-12-27 20:09:29 +0100

    Plantillas para correo electrónico
    Se han creado plantillas para el mensaje de correo electrónico y se han
    realizado algunos cambios en su implementación.

--- decide/authentication/email.py
+++ decide/authentication/email.py
@@ -2,17 +2,16 @@ from django.core.mail import EmailMultiAlternatives, BadHeaderError
 from django.template.loader import get_template
 from django.template import Context
 
-def send_mail_with_token(to_email, token):
-    subject = 'Decide - Correo de prueba'
-    from_email = 'Decide <do_not_reply@example.com>'
+def send_mail_with_token(to_email, link):
+    subject = 'Decide - Correo para autenticación'
+    from_email = 'Decide <do_not_reply@decide.org>'
 
-    cont = { 
-        'usuario': 'username de prueba',
-        'token': token
+    cont = {
+        'link': link
     }
 
-    msg_plain = get_template('email.txt').render(cont)
-    msg_html = get_template('email.html').render(cont)
+    msg_plain = get_template('authentication/email/email.txt').render(cont)
+    msg_html = get_template('authentication/email/email.html').render(cont)
 
     try:
         msg = EmailMultiAlternatives(subject, msg_plain, from_email, [to_email])
