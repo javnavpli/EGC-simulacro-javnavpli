cf2a8e31d84d25f43ee40e7c6158407663b2c91a
Pablo Rodriguez Garrido <56022210+PabloRodriguezGarrido11@users.noreply.github.com>
2021-01-02 10:46:15 +0100

    Update views.py

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -2,10 +2,11 @@ from rest_framework import parsers, renderers
 from authentication.models import EmailOTPCode
 from rest_framework.response import Response
 from rest_framework.status import (
-        HTTP_200_OK, HTTP_201_CREATED,
+        HTTP_201_CREATED,
         HTTP_400_BAD_REQUEST,
         HTTP_200_OK,
-        HTTP_401_UNAUTHORIZED, HTTP_500_INTERNAL_SERVER_ERROR
+        HTTP_401_UNAUTHORIZED, 
+        HTTP_500_INTERNAL_SERVER_ERROR
 
 )
 from  smtplib import SMTPException
