bba1455a27c8e764de94b9f0d8a1395e809029bc
Pablo Rodriguez Garrido <56022210+PabloRodriguezGarrido11@users.noreply.github.com>
2021-01-02 10:45:37 +0100

    Update views.py

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.status import (
         HTTP_200_OK, HTTP_201_CREATED,
         HTTP_400_BAD_REQUEST,
-        HTTP_200_OK
+        HTTP_200_OK,
         HTTP_401_UNAUTHORIZED, HTTP_500_INTERNAL_SERVER_ERROR
 
 )
